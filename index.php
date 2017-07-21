6df3a5a967c538885094bf8ae247bad20e649fe1
leonlau95 <leonlau95@gmail.com>
2017-07-21 13:54:32 +0800

    205CDE

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php 
+/* Main page with two forms: sign up and log in */
+require 'db.php';
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sign-Up/Login Form</title>
+  <?php include 'css/css.html'; ?>
+</head>
+
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
+    if (isset($_POST['login'])) { //user logging in
+
+        require 'login.php';
+        
+    }
+    
+    elseif (isset($_POST['register'])) { //user registering
+        
+        require 'register.php';
+        
+    }
+}
+?>
+<body>
+	<header id="header" class="layers">
+        <h1><a href="japan.html">Welcome to Japan Youtuber Channel!!</a></h1>	
+    </header>
+    <div class="form">
+		<ul class="tab-group">
+        <li class="tab"><a href="#signup">Sign Up</a></li>
+        <li class="tab active"><a href="#login">Log In</a></li>
+    </ul>
+      
+    <div class="tab-content">
+
+        <div id="login">   
+        <h1>Welcome Back!</h1>  
+        <form action="index.php" method="post" autocomplete="off">
+		<div class="field-wrap">
+		<label>
+		Please your Email Address<span class="req">*</span>
+		</label>
+			<input type="email" required autocomplete="off" name="email"/>
+        </div>
+		
+        <div class="field-wrap">
+            <label>
+              Please your password<span class="req">*</span>
+            </label>
+            <input type="password" required autocomplete="off" name="password"/>
+          </div>
+          
+          <p class="forgot"><a href"#">Forgot Your Password?</a></p>
+          
+          <button class="button button-block" name="login" />Log In</button>
+          
+          </form>
+
+        </div>
+          
+        <div id="signup">   
+          <h1>Register for Free</h1>
+          
+          <form action="index.php" method="post" autocomplete="off">
+          
+          <div class="top-row">
+            <div class="field-wrap">
+              <label>
+                First Name<span class="req">*</span>
+              </label>
+              <input type="text" required autocomplete="off" name='firstname' />
+            </div>
+        
+            <div class="field-wrap">
+              <label>
+                Last Name<span class="req">*</span>
+              </label>
+              <input type="text"required autocomplete="off" name='lastname' />
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Please enter your email address<span class="req">*</span>
+            </label>
+            <input type="email"required autocomplete="off" name='email' />
+          </div>
+          
+          <div class="field-wrap">
+            <label>
+              Please set your Password<span class="req">*</span>
+            </label>
+            <input type="password"required autocomplete="off" name='password'/>
+          </div>
+          
+          <button type="submit" class="button button-block" name="register" />Register</button>
+          
+          </form>
+
+        </div>  
+        
+      </div><!-- tab-content -->
+      
+</div> <!-- /form -->
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+</body>
+</html>
